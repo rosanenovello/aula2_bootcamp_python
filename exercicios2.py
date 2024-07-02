97e6b6579cb3866cafc75c383e6904451cd113ab
Rosane Novello <rosanenovello@gmail.com>
2024-07-02 19:34:50 -0300

    Exercicios com booleano

--- exercicios2.py
+++ exercicios2.py
@@ -19,12 +19,33 @@
 ###4) Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 #input("Insira uma data no formato dd/mmm/aaaa: ")
 
-data_do_usuario = "20/12/2024"
-data_separada = data_do_usuario.split("/")
-print(f"Data Informada: {data_separada}")
-print(f"Dia: {data_separada[0]}")
-print(f"Mes: {data_separada[1]}")
-print(f"Ano: {data_separada[2]}")
+# data_do_usuario = "20/12/2024"
+# data_separada = data_do_usuario.split("/")
+# print(f"Data Informada: {data_separada}")
+# print(f"Dia: {data_separada[0]}")
+# print(f"Mes: {data_separada[1]}")
+# print(f"Ano: {data_separada[2]}")
 
 
 #5) Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+
+
+################ Booleanos (bool)
+#1) Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+#2) Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+#3) Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+#4) Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+#5) Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+
+exp1 = True
+exp2 = False
+
+n1=3
+n2=3
+
+print(f"O resultado do AND é {exp1 and exp2}")
+print(f"O resultado do OR é {exp1 or exp2}")
+print(f"FALSE invertido {not exp2}")
+print(f"Compara {n1} com {n2}  e o resultado é {n1 == n2}")
+
