f9c2822df305b672b0a215321614697c8c20f39c
Rosane Novello <rosanenovello@gmail.com>
2024-07-02 13:32:08 -0300

    Exercicios com int

---
+++ exercicios.py
@@ -0,0 +1,31 @@
+#Inteiros (int)
+####Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+#v1 = int(input("Forneça o primeiro numero inteiro: "))
+#v2 = int(input("Forneça o primeiro numero inteiro: "))
+       
+#print(f"A soma dos dois numeros inteiros é {v1 + v2}")       
+
+####Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+#v1 = int(input("Forneça o primeiro numero inteiro: "))
+
+#print(f"O resto da divisao deste numero por 5 é {v1 % 5}")       
+
+####Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+#v1 = int(input("Forneça o primeiro numero inteiro: "))
+#v2 = int(input("Forneça o primeiro numero inteiro: "))
+       
+#print(f"A multiplicacap dos dois numeros inteiros é {v1 * v2}")       
+
+
+##Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+#v1 = int(input("Forneça o primeiro numero inteiro: "))
+#v2 = int(input("Forneça o primeiro numero inteiro: "))
+       
+#print(f"A divisao dos dois numeros inteiros é {v1 // v2}")       
+
+
+##Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+v1 = int(input("Forneça o primeiro numero inteiro: "))
+
+       
+print(f"O quadrado é {v1 ** 2}")    
