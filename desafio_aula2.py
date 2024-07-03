9a85a5e66e0c499dd5283fd95da2d6ccbdb75e9f
Rosane Novello <rosanenovello@gmail.com>
2024-07-03 19:32:30 -0300

    Validacoes de erro de string

--- desafio_aula2.py
+++ desafio_aula2.py
@@ -45,26 +45,30 @@
 # por zero e entradas não numéricas. Utilize if-elif-else para realizar a operação matemática baseada no operador fornecido. Imprima o resultado ou uma
 #  mensagem de erro apropriada.
 
-try:
-    numero1 = int(input("Digite um numero inteiro: "))
-    numero2 = int(input("Digite um numero inteiro: "))
-    try:
-        oper = input("Digite a operacao desejada SOMA SUBTRACAO DIVISAO MULTIPLICACAO, utilize os operadores (+, -, *, /) para indica: ")
-        if oper == "+":
-           print(f"Resultado da soma de {numero1} pelo {numero2} é {numero1 + numero2}")
-        elif oper == "-":
-           print(f"Resultado da soma de {numero1} pelo {numero2} é {numero1 - numero2}")
-        elif oper == "*":
-           print(f"Resultado da soma de {numero1} pelo {numero2} é {numero1 * numero2}")
-        elif oper == "/" and numero2 != 0:
-           print(f"Resultado da soma de {numero1} pelo {numero2} é {numero1 / numero2}")
-        else:
-           print("Operacao Desconhecida ou Divisao por ZERO")   
-    except: 
-        print("Erro na Operacao")   
+# try:
+#     numero1 = int(input("Digite um numero inteiro: "))
+#     numero2 = int(input("Digite um numero inteiro: "))
+#     if (numero1 % 2):
+#        print(f"Primeiro numero é PAR")
+#     else:
+#        print(f"Primeiro numero é IMPAR")
+#     if (numero1 < 0 or numero2 < 0): 
+#        print("Numeros NEGATIVOS nao permitidos")
+#     else:
+#         oper = input("Digite a operacao desejada SOMA SUBTRACAO DIVISAO MULTIPLICACAO, utilize os operadores (+, -, *, /) para indica: ")
+#         if oper == "+":
+#            print(f"Resultado da soma de {numero1} pelo {numero2} é {numero1 + numero2}")
+#         elif oper == "-":
+#            print(f"Resultado da soma de {numero1} pelo {numero2} é {numero1 - numero2}")
+#         elif oper == "*":
+#            print(f"Resultado da soma de {numero1} pelo {numero2} é {numero1 * numero2}")
+#         elif oper == "/" and numero2 != 0:
+#            print(f"Resultado da soma de {numero1} pelo {numero2} é {numero1 / numero2}")
+#         else:
+#            print("Operacao Desconhecida ou Divisao por ZERO")   
 
-except ValueError:
-    print("Somente numero serão aceitos")   
+# except ValueError:
+#     print("Somente numero serão aceitos")   
 
 
 # Exercício 24: Classificador de Números
@@ -75,3 +79,32 @@ except ValueError:
 # Crie um script que solicite ao usuário uma lista de números separados por vírgula. O programa deve converter a string de entrada em uma lista de números inteiros. 
 # Utilize try-except para tratar a conversão de cada número e validar que cada elemento da lista convertida é um inteiro. Se a conversão falhar ou um elemento não for um
 #  inteiro, imprima uma mensagem de erro. Se a conversão for bem-sucedida para todos os elementos, imprima a lista de inteiros.
+
+# entrada_lista = input("Digite uma lista de números separados por vírgula: ")
+# numeros_str = entrada_lista.split(",")
+# print(f"entrada { entrada_lista}")
+# print(f"depois do split { numeros_str}")
+# numeros_int = []
+# try:
+#     for num in numeros_str:
+#         numeros_int.append(int(num.strip()))
+#     print("Lista de inteiros:", numeros_int)
+# except ValueError:
+#     print("Erro: certifique-se de que todos os elementos são números inteiros válidos.")    
+
+##############################################################################################
+## Exercicio Extra de tratamento de erros
+
+nome = input("Digite seu nome: ")
+
+if nome.isdigit():
+   print("Voce digitou seu nome errado")
+   exit()
+elif len(nome) == 0:
+   print("Voce NAo digitou nada")   
+   exit()
+elif nome.isspace():   
+   print("Voce digitou somente espacos")   
+   exit()
+else:
+   print(f"Seu nome {nome}")  
