db31bd7872294068f05314ad87b565111cf86686
Rosane Novello <rosanenovello@gmail.com>
2024-07-03 19:56:57 -0300

    lista de inteiros

--- desafio_aula2.py
+++ desafio_aula2.py
@@ -80,31 +80,31 @@
 # Utilize try-except para tratar a conversão de cada número e validar que cada elemento da lista convertida é um inteiro. Se a conversão falhar ou um elemento não for um
 #  inteiro, imprima uma mensagem de erro. Se a conversão for bem-sucedida para todos os elementos, imprima a lista de inteiros.
 
-# entrada_lista = input("Digite uma lista de números separados por vírgula: ")
-# numeros_str = entrada_lista.split(",")
-# print(f"entrada { entrada_lista}")
-# print(f"depois do split { numeros_str}")
-# numeros_int = []
-# try:
-#     for num in numeros_str:
-#         numeros_int.append(int(num.strip()))
-#     print("Lista de inteiros:", numeros_int)
-# except ValueError:
-#     print("Erro: certifique-se de que todos os elementos são números inteiros válidos.")    
+entrada_lista = input("Digite uma lista de números separados por vírgula: ")
+numeros_str = entrada_lista.split(",")
+print(f"entrada { entrada_lista}")
+print(f"depois do split { numeros_str}")
+numeros_int = []
+try:
+    for num in numeros_str:
+        numeros_int.append(int(num.strip()))
+    print("Lista de inteiros:", numeros_int)
+except ValueError:
+    print("Erro: certifique-se de que todos os elementos são números inteiros válidos.")    
 
 ##############################################################################################
 ## Exercicio Extra de tratamento de erros
 
-nome = input("Digite seu nome: ")
+# nome = input("Digite seu nome: ")
 
-if nome.isdigit():
-   print("Voce digitou seu nome errado")
-   exit()
-elif len(nome) == 0:
-   print("Voce NAo digitou nada")   
-   exit()
-elif nome.isspace():   
-   print("Voce digitou somente espacos")   
-   exit()
-else:
-   print(f"Seu nome {nome}")  
+# if nome.isdigit():
+#    print("Voce digitou seu nome errado")
+#    exit()
+# elif len(nome) == 0:
+#    print("Voce NAo digitou nada")   
+#    exit()
+# elif nome.isspace():   
+#    print("Voce digitou somente espacos")   
+#    exit()
+# else:
+#    print(f"Seu nome {nome}")  
